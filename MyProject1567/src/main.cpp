b5b586fb8ab6c3f9759504503e61d0b8c6b09552
shiori1024 <127829032+shiori1024@users.noreply.github.com>
2023-03-14 16:21:20 +0900

    initialize

---
+++ MyProject1567/src/main.cpp
@@ -0,0 +1,63 @@
+
+#include "vex.h"
+
+using namespace vex;
+
+controller ctrl;
+competition Comp;
+
+motor fRight (PORT10);
+motor fLeft (PORT3,true);
+motor bRight (PORT20);
+motor bLeft (PORT12,true);
+motor_group cLeft (fLeft, bLeft);
+motor_group cRight (fRight, bRight);
+
+float m = 1;
+int toggle = false;
+bool block = false; 
+
+void userCtrl(){
+while (true){ 
+
+    if (ctrl.ButtonA.pressing()&& block==true){
+      toggle = !toggle;
+      block = false;
+    }
+    if (!ctrl.ButtonA.pressing()){
+      block = true;
+    }
+    m = (toggle) ? 0.5:1;
+    
+    cRight.spin (directionType::fwd, ctrl.Axis2.value() * m, velocityUnits::pct);
+    cLeft.spin (directionType::fwd, ctrl.Axis3.value() * m, velocityUnits::pct);
+    
+    task::sleep(20);
+
+  }
+}
+
+void auton(){
+
+  cRight.spin (directionType::fwd, 50, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, 50, velocityUnits::pct);
+  task::sleep (4000);
+
+  cRight.spin (directionType::fwd, 20, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, -20, velocityUnits::pct);
+  task::sleep (1500);
+
+  cRight.spin (directionType::fwd, -100, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, -100, velocityUnits::pct);
+  task::sleep (2000);
+
+  cRight.stop();
+  cLeft.stop();
+}
+
+int main() {
+  vexcodeInit();
+  Comp.autonomous (auton);
+  Comp.drivercontrol(userCtrl);
+}
+
